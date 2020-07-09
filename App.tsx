17401fede66e413b2fb0b8e84612aeb464946b2f
Matheus Franco <matheusfrancoalves@hotmail.com>
2020-07-09 12:19:04 -0300

    Clean project setup and add linting

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>GoFinances Mobile</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <View>
+            <Text>GoFinances - mobile</Text>
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E5E5E5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
