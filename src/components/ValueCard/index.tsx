e8e1276b0a122490017cfd1d08da997200fa8de5
Matheus Kuster <matheuskuster@hotmail.com>
2020-07-10 11:32:25 -0300

    Update ValueCard and add its behavior

--- src/components/ValueCard/index.tsx
+++ src/components/ValueCard/index.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { Feather } from '@expo/vector-icons';
 
-import {
-  Container,
-  ValueCardTotal,
-  ValueCardTitle,
-  SpaceBetweenDiv,
-} from './styles';
+import { Container, DescriptionIcon, Description, Price } from './styles';
+
+interface IIcon {
+  name: string;
+  color: string;
+}
+
+const cardTypes = ['Total', 'Entradas', 'Saídas'];
 
 const ValueCard: React.FC = () => {
-  return (
-    <Container>
-      <SpaceBetweenDiv>
-        <ValueCardTitle>Total</ValueCardTitle>
-        <ValueCardTitle>$</ValueCardTitle>
-      </SpaceBetweenDiv>
+  const [cardType, setCardType] = useState<number>(0);
 
-      <ValueCardTotal>R$ 24.000,00</ValueCardTotal>
+  function handleClick(): void {
+    if (cardType === 2) {
+      setCardType(0);
+    } else {
+      setCardType(cardType + 1);
+    }
+  }
+
+  const icon = useMemo((): IIcon => {
+    if (cardType === 0) return { name: 'dollar-sign', color: '#fff' };
+    if (cardType === 1) return { name: 'arrow-up-circle', color: '#12A454' };
+    return { name: 'arrow-down-circle', color: '#E83F5B' };
+  }, [cardType]);
+
+  return (
+    <Container activeOpacity={1} onPress={handleClick} active={cardType === 0}>
+      <DescriptionIcon>
+        <Description active={cardType === 0}>{cardTypes[cardType]}</Description>
+        <Feather name={icon.name} color={icon.color} size={24} />
+      </DescriptionIcon>
+      <Price active={cardType === 0}>R$24.000,00</Price>
     </Container>
   );
 };
