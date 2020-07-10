360ca167c1d73c59d578bec17ca6bedca4303805
Matheus Kuster <matheuskuster@hotmail.com>
2020-07-10 11:33:22 -0300

    Update Content to reflect on new changes

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
+import ValueCard from '../ValueCard';
 
 import Transaction from '../Transaction';
 
@@ -8,6 +9,7 @@ import { Container } from './styles';
 const Content: React.FC = () => {
   return (
     <Container>
+      <ValueCard />
       <ScrollView>
         <Transaction />
       </ScrollView>
