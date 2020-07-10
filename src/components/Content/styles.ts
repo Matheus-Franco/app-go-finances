9a9d700efa76644747a3eb7737b7a74d83fa9c6e
Matheus Kuster <matheus.kuster@vulcanet.com.br>
2020-07-10 11:33:22 -0300

    Update Content to reflect on new changes

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -3,7 +3,5 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background: #e5e5e5;
-
-  padding-top: 100px;
-  align-items: center;
+  padding: 0 20px;
 `;
