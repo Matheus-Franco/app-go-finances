e60404fb1d8afbd3a97997f24605716b49d51dd0
Matheus Kuster <matheus.kuster@vulcanet.com.br>
2020-07-10 11:33:08 -0300

    Update Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Feather } from '@expo/vector-icons';
+import logo from '../../assets/logo.png';
 
-import ValueCard from '../ValueCard';
-
-import { Container, Title } from './styles';
+import { Container, Logo } from './styles';
 
 const Header: React.FC = () => {
   return (
     <Container>
-      <Title>
-        <Feather name="dollar-sign" size={32} color="#FE883B" />
-        gofinances
-      </Title>
-      <ValueCard />
+      <Logo source={logo} />
     </Container>
   );
 };
