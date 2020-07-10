d15e5c4ede3a90ec8578a67e716c77fb4375637e
Matheus Kuster <matheuskuster@hotmail.com>
2020-07-10 11:29:56 -0300

    Load Poppins font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 import Header from './src/components/Header';
 import Content from './src/components/Content';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins: Poppins_400Regular,
+    PoppinsBold: Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) return <View />;
+
   return (
     <>
       <StatusBar barStyle="light-content" />
