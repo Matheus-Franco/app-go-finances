be5996f6af99b01f8caf3308654bb11cef7a6eff
Matheus Kuster <matheuskuster@hotmail.com.br>
2020-07-11 10:19:13 -0300

    Update Content to add footer and fix scroll view position

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -3,6 +3,7 @@ import { ScrollView } from 'react-native';
 import ValueCard from '../ValueCard';
 
 import Transaction from '../Transaction';
+import Footer from '../Footer';
 
 import { Container } from './styles';
 
@@ -10,11 +11,20 @@ const Content: React.FC = () => {
   return (
     <Container>
       <ValueCard />
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={[
+          { marginLeft: 20 },
+          {
+            transform: [{ translateY: -62 }],
+          },
+        ]}
+      >
         <Transaction />
         <Transaction />
         <Transaction />
       </ScrollView>
+      <Footer />
     </Container>
   );
 };
--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -3,5 +3,4 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background: #e5e5e5;
-  padding: 0 20px;
 `;
