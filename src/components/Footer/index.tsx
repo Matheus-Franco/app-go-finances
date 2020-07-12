63cb94c0c7eae85d2ce9763638e7ae7b9f374775
Matheus Franco <matheusfrancoalves@hotmail.com>
2020-07-12 19:40:44 -0300

    Add form in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,80 @@
 import React from 'react';
+import { View } from 'react-native';
 
-import { Container, Button, ButtonText } from './styles';
+import {
+  Container,
+  ButtonArea,
+  Button,
+  ButtonText,
+  Form,
+  FormArea,
+  Icon,
+  Input,
+  Title,
+  InputArea,
+} from './styles';
 
 const Footer: React.FC = () => {
   return (
     <Container>
-      <Button color="#5537CE;">
-        <ButtonText>Paguei</ButtonText>
-      </Button>
-      <Button color="#FE883B;">
-        <ButtonText>Recebi</ButtonText>
-      </Button>
+      <ButtonArea direction="row">
+        <Button widthSize={180} color="#5537CE">
+          <ButtonText>Paguei</ButtonText>
+        </Button>
+        <Button widthSize={180} color="#FE883B">
+          <ButtonText>Recebi</ButtonText>
+        </Button>
+      </ButtonArea>
+
+      <Form>
+        <FormArea>
+          <View style={{ width: '70%' }}>
+            <Title>Valor</Title>
+          </View>
+          <View style={{ width: '30%' }}>
+            <Input placeholder="R$0.00" />
+          </View>
+        </FormArea>
+
+        <FormArea>
+          <View>
+            <Title>Título</Title>
+            <InputArea>
+              <Icon name="edit-2" color="#363f5f" size={24} />
+              <Input placeholder="Adicione o título do lançamento" />
+            </InputArea>
+          </View>
+        </FormArea>
+
+        <FormArea>
+          <View>
+            <Title>Categoria</Title>
+            <InputArea>
+              <Icon name="list" color="#363f5f" size={24} />
+              <Input placeholder="Outros" />
+            </InputArea>
+          </View>
+        </FormArea>
+
+        <FormArea>
+          <View>
+            <Title>Data</Title>
+            <InputArea>
+              <Icon name="calendar" color="#363f5f" size={24} />
+              <Input placeholder="Hoje" />
+            </InputArea>
+          </View>
+        </FormArea>
+      </Form>
+
+      <ButtonArea direction="column">
+        <Button widthSize={370} color="#12A454">
+          <ButtonText>Adicionar</ButtonText>
+        </Button>
+        <Button widthSize={370} color="#E83F5B">
+          <ButtonText>Cancelar</ButtonText>
+        </Button>
+      </ButtonArea>
     </Container>
   );
 };
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,7 +1,17 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
 
 interface IButton {
   color: string;
+  widthSize: number;
+}
+
+interface IButtonArea {
+  direction: string;
+}
+
+interface ITextInput {
+  fSize?: number;
 }
 
 export const Container = styled.View`
@@ -10,23 +20,66 @@ export const Container = styled.View`
   z-index: 300;
   box-shadow: 0px -1px 12px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
+`;
 
-  flex-direction: row;
+export const ButtonArea = styled.View<IButtonArea>`
+  flex-direction: ${props => props.direction};
   padding: 20px;
   align-items: center;
   justify-content: space-between;
+  height: 85px;
 `;
 
 export const Button = styled.TouchableOpacity<IButton>`
   background: ${props => props.color};
-  width: 48%;
+  width: ${props => props.widthSize};
   align-items: center;
   justify-content: center;
   height: 100%;
   border-radius: 10px;
+  margin-bottom: 5px;
 `;
 
 export const ButtonText = styled.Text`
   color: #fff;
   font-family: 'PoppinsBold';
+`;
+
+export const Form = styled.View`
+  height: 300px;
+  margin: 0 20px 100px;
+`;
+
+export const InputArea = styled.View`
+  flex-direction: row;
+`;
+
+export const FormArea = styled.View`
+  height: 90px;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+
+  background: transparent;
+  border-bottom-width: 1px;
+  border-color: #f0f2f5;
+  padding: 8px;
+`;
+
+export const Icon = styled(Feather)`
+  margin-right: 8px;
+`;
+
+export const Input = styled.TextInput<ITextInput>`
+  font-family: 'Poppins';
+  font-size: 16px;
+  color: #363f5f;
+  width: 90%;
+`;
+
+export const Title = styled.Text`
+  font-family: 'Poppins';
+  font-size: 28px;
+  color: #363f5f;
+  width: 50%;
 `;
